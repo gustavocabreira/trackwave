8de8747f7ed163be9bd7b42e91a177da38739e9c
Gustavo Cabreira <gustavo.softdev@gmail.com>
2025-09-23 21:43:19 -0300

    wip

--- docker/testing/set_storage.sh
+++ docker/testing/set_storage.sh
@@ -6,10 +6,10 @@ docker volume create ${APP_NAME}-laravel_storage
 MOUNTPOINT=$(docker volume inspect ${APP_NAME}-laravel_storage --format='{{.Mountpoint}}')
 
 # Check if the directory in the volume is empty
-if [ -z "$(sudo ls -A "$MOUNTPOINT")" ]; then
+if [ -z "$(ls -A "$MOUNTPOINT")" ]; then
     # If empty, copy files into it
-    sudo cp -r ../../storage/. "$MOUNTPOINT"
-    sudo chown -R $USER_ID:$USER_GROUP "$MOUNTPOINT"
+    cp -r ../../storage/. "$MOUNTPOINT"
+    chown -R $USER_ID:$USER_GROUP "$MOUNTPOINT"
     echo "Files copied to $VOLUME_NAME volume."
 else
     echo "Directory in $VOLUME_NAME volume is not empty."
