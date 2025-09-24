49159e4e71bbdfac370317a1a3df8a2a2cce5c01
Gustavo Cabreira <gustavo.softdev@gmail.com>
2025-09-23 21:55:57 -0300

    wip

--- docker/testing/set_storage.sh
+++ docker/testing/set_storage.sh
@@ -1,19 +1,16 @@
 #!/bin/sh
-set -e
 
-VOLUME_NAME="${APP_NAME}-laravel_storage"
+docker volume create ${APP_NAME}-laravel_storage
 
-docker volume create $VOLUME_NAME
+# Get the mount point of the Docker volume
+MOUNTPOINT=$(docker volume inspect ${APP_NAME}-laravel_storage --format='{{.Mountpoint}}')
 
-# Verifica se volume está vazio
-IS_EMPTY=$(docker run --rm -v $VOLUME_NAME:/data alpine sh -c "[ -z \"\$(ls -A /data)\" ] && echo empty")
-
-if [ "$IS_EMPTY" = "empty" ]; then
-  echo "Seeding storage into $VOLUME_NAME..."
-  docker run --rm \
-    -v $VOLUME_NAME:/dest \
-    -v $(pwd)/../../storage:/src:ro \
-    alpine sh -c "cp -a /src/. /dest/ && chown -R $USER_ID:$GROUP_ID /dest"
+# Check if the directory in the volume is empty
+if [ -z "$(ls -A "$MOUNTPOINT")" ]; then
+    # If empty, copy files into it
+    cp -r ../../storage/. "$MOUNTPOINT"
+    chown -R $USER_ID:$USER_GROUP "$MOUNTPOINT"
+    echo "Files copied to $VOLUME_NAME volume."
 else
-  echo "Volume $VOLUME_NAME já contém arquivos, não vou sobrescrever."
+    echo "Directory in $VOLUME_NAME volume is not empty."
 fi
