35e5888e1c6264a644145aa17950b8b1b18810a7
Gustavo Cabreira <gustavo.softdev@gmail.com>
2025-09-25 20:24:19 -0300

    wip

--- routes/api.php
+++ routes/api.php
@@ -4,11 +4,23 @@ declare(strict_types=1);
 
 use App\Http\Controllers\Api\Auth\LoginController;
 use App\Http\Controllers\Api\Auth\RegisterController;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 
 Route::name('api.')->group(function () {
     Route::prefix('auth')->name('auth.')->group(function () {
         Route::post('register', [RegisterController::class, 'store'])->name('register');
         Route::post('login', [LoginController::class, 'store'])->name('login');
+        Route::post('logout', function() {
+            request()->user()->tokens()->delete();
+            request()->session()->invalidate();
+            request()->session()->regenerateToken();
+
+            return response()->json(null, Response::HTTP_NO_CONTENT);
+        });
     });
+
+    Route::middleware('auth:sanctum')->get('me', function() {
+        return request()->user();
+    })->name('me');
 });
