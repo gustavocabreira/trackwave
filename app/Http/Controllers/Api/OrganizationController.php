7be60da9c7f06ccbbdca3db080ee9bd7e083c537
Gustavo Cabreira <gustavo.softdev@gmail.com>
2025-10-04 19:08:06 -0300

    feat(organizations): it should be able to paginate the user organizations list

--- app/Http/Controllers/Api/OrganizationController.php
+++ app/Http/Controllers/Api/OrganizationController.php
@@ -6,17 +6,22 @@ namespace App\Http\Controllers\Api;
 
 use App\Actions\Organization\CreateOrganizationAction;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Organization\IndexOrganizationRequest;
 use App\Http\Requests\Organization\StoreOrganizationRequest;
 use App\Http\Resources\OrganizationResource;
 
 final class OrganizationController extends Controller
 {
-    public function index()
+    /**
+     * Get a list of the user organizations
+     */
+    public function index(IndexOrganizationRequest $request)
     {
         $organizations = auth()->user()->organizations()->with('owner')->paginate(10);
+
         return OrganizationResource::collection($organizations);
     }
-    
+
     /**
      * Store a new organization
      */
--- tests/Feature/Api/Organization/IndexOrganizationTest.php
+++ tests/Feature/Api/Organization/IndexOrganizationTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use App\Models\Organization;
 use App\Models\User;
 
@@ -52,14 +54,33 @@ it('should return only the user organizations', function () {
 
     $response = $this->actingAs($user)->getJson(route('api.organizations.index'));
 
-    foreach($response->json('data') as $organization) {
+    foreach ($response->json('data') as $organization) {
         expect($organization['id'])->not->toBe($anotherOrganization->id);
     }
-    
+
     expect($response->json('meta.current_page'))->toBe(1)
         ->and($response->json('meta.from'))->toBe(1)
         ->and($response->json('meta.last_page'))->toBe(1)
         ->and($response->json('meta.per_page'))->toBe(10)
         ->and($response->json('meta.to'))->toBe(1)
         ->and($response->json('meta.total'))->toBe(1);
+});
+
+it('should be able to paginate', function () {
+    $user = User::factory()->create();
+    $organizations = Organization::factory()->count(15)->create(['owner_id' => $user->id]);
+    $user->organizations()->attach($organizations);
+
+    $response = $this->actingAs($user)->getJson(route('api.organizations.index', [
+        'page' => 2,
+    ]));
+
+    $response->assertOk()->assertJsonCount(5, 'data');
+
+    expect($response->json('meta.current_page'))->toBe(2)
+        ->and($response->json('meta.from'))->toBe(11)
+        ->and($response->json('meta.last_page'))->toBe(2)
+        ->and($response->json('meta.per_page'))->toBe(10)
+        ->and($response->json('meta.to'))->toBe(15)
+        ->and($response->json('meta.total'))->toBe(15);
 });
