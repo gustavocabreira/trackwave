0eb69fc7bcd4d98f82314b059adabe3379dcbf37
Gustavo Cabreira <gustavo.softdev@gmail.com>
2025-10-04 19:04:45 -0300

    feat(organizations): it should return only the user organizations

--- tests/Feature/Api/Organization/IndexOrganizationTest.php
+++ tests/Feature/Api/Organization/IndexOrganizationTest.php
@@ -40,3 +40,26 @@ it('should return a list of the user organizations', function () {
         ->and($response->json('meta.to'))->toBe(10)
         ->and($response->json('meta.total'))->toBe(15);
 });
+
+it('should return only the user organizations', function () {
+    $user = User::factory()->create();
+    $organization = Organization::factory()->create(['owner_id' => $user->id]);
+    $user->organizations()->attach($organization);
+
+    $anotherUser = User::factory()->create();
+    $anotherOrganization = Organization::factory()->create(['owner_id' => $anotherUser->id]);
+    $anotherUser->organizations()->attach($anotherOrganization);
+
+    $response = $this->actingAs($user)->getJson(route('api.organizations.index'));
+
+    foreach($response->json('data') as $organization) {
+        expect($organization['id'])->not->toBe($anotherOrganization->id);
+    }
+    
+    expect($response->json('meta.current_page'))->toBe(1)
+        ->and($response->json('meta.from'))->toBe(1)
+        ->and($response->json('meta.last_page'))->toBe(1)
+        ->and($response->json('meta.per_page'))->toBe(10)
+        ->and($response->json('meta.to'))->toBe(1)
+        ->and($response->json('meta.total'))->toBe(1);
+});
