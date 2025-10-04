df9634dc82c15319e3440ea27ad47a1870a2b7a8
Gustavo Cabreira <gustavo.softdev@gmail.com>
2025-10-04 18:54:01 -0300

    feat(organizations): refactor into action class

--- app/Http/Controllers/Api/OrganizationController.php
+++ app/Http/Controllers/Api/OrganizationController.php
@@ -4,29 +4,18 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\Api;
 
+use App\Actions\Organization\CreateOrganizationAction;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Organization\StoreOrganizationRequest;
-use App\Models\Organization;
 
 final class OrganizationController extends Controller
 {
-    public function store(StoreOrganizationRequest $request)
+    /**
+     * Store a new organization
+     */
+    public function store(StoreOrganizationRequest $request, CreateOrganizationAction $action)
     {
-        $slug = $request->input('slug');
-
-        if($slug === null) {
-            $slug = now()->timestamp . '-' . str()->slug($request->string('name'));
-        }
-
-        $slug = str()->substr($slug, 0, 50);
-
-        $organization = Organization::query()->create([
-            'name' => $request->string('name'),
-            'slug' => $slug,
-            'owner_id' => auth()->user()->id,
-        ]);
-
-        auth()->user()->organizations()->attach($organization);
+        $organization = $action->execute($request->validated());
 
         return $organization->toResource();
     }
