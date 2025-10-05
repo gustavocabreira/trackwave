f565b907bef209954c63c0da39bea936e0f31223
Gustavo Cabreira <gustavo.softdev@gmail.com>
2025-10-05 16:17:39 -0300

    feat(projects): should return unprocessable entity when payload is invalid

--- tests/Feature/Api/Project/StoreProjectTest.php
+++ tests/Feature/Api/Project/StoreProjectTest.php
@@ -41,3 +41,40 @@ it('should be able to store a project', function () {
 
     $this->assertDatabaseCount('projects', 1);
 });
+
+dataset('invalid_payload', [
+    'invalid name' => [
+        ['name' => ''], ['name' => ['The name field is required.']],
+    ],
+    'name with less than 3 characters' => [
+        ['name' => str()->random(2)], ['name' => ['The name field must be at least 3 characters.']],
+    ],
+    'name with more than 100 characters' => [
+        ['name' => str()->random(101)], ['name' => ['The name field must not be greater than 100 characters.']],
+    ],
+    'description with less than 3 characters' => [
+        ['description' => str()->random(2)], ['description' => ['The description field must be at least 3 characters.']],
+    ],
+    'description with more than 255 characters' => [
+        ['description' => str()->random(256)], ['description' => ['The description field must not be greater than 255 characters.']],
+    ],
+]);
+
+it('should return unprocessable entity when payload is invalid', function (array $payload, array $expectedErrors) {
+    $user = User::factory()->create();
+    $payload = Organization::factory()->make(['owner_id' => $user->id, ...$payload])->toArray();
+
+    $user = User::factory()->create();
+    $organization = Organization::factory()->create();
+    $user->organizations()->attach($organization);
+
+    $response = $this->actingAs($user)->postJson(route('api.organizations.projects.store', [
+        'organization' => $organization->id,
+    ]), $payload);
+
+    $response
+        ->assertUnprocessable()
+        ->assertJsonFragment([
+            'errors' => $expectedErrors,
+        ]);
+})->with('invalid_payload');
